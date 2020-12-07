2400bbb3bdfa708d8c113ae130fafb2cde00665b
lord-gaurav-first <75430381+lord-gaurav-first@users.noreply.github.com>
2020-12-07 22:57:54 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -45,9 +45,15 @@ function draw() {
   drawSprites();
   
   monkey.collide(ground);
+  
+    stroke("black");
+    fill("black");
+    textSize(22);
+    text("Survival Time: " + score, 200, 60);
+  
   if (gameState=== PLAY){
       
-    if (keyDown("space") && monkey.y >= 360 && gameState=== PLAY) {
+    if (keyDown("space") && monkey.y >= 360) {
      monkey.velocityY= -13;
     }
 
@@ -62,48 +68,38 @@ function draw() {
      bananas();
      obstacles();
 
-     stroke("black");
-     fill("black");
-     textSize(22);
-     text("Survival Time: " + score, 200, 60);
-
      score= Math.round(frameCount/60);
     
     if (obstaclesGroup.isTouching(monkey)){
         gameState= END;
-      
-        monkey.pause();
-      
-        obstaclesGroup.setVelocityXEach(0);
-        fruitsGroup.setVelocityXEach(0);
-      
-        ground.velocityX= 0;
-
-        //set lifetime of the game objects so that they are never destroyed
-        obstaclesGroup.setLifetimeEach(-1);
-        fruitsGroup.setLifetimeEach(-1);
-
-       score= 0;
      }
     
     if (fruitsGroup.isTouching(monkey)) {
-      fruitsGroup.destroyEach();
+       fruitsGroup.destroyEach();
     }
     
-  }else if(gameState=== END) {
+  }else if(gameState=== END) {  
+     monkey.pause();
+      
+     obstaclesGroup.setVelocityXEach(0);
+     fruitsGroup.setVelocityXEach(0);
+      
+     ground.velocityX= 0;
 
+     //set lifetime of the game objects so that they are never destroyed
+     obstaclesGroup.setLifetimeEach(-1);
+     fruitsGroup.setLifetimeEach(-1);
   }
 }
 
 function bananas(){
   if (frameCount%80=== 0) {
-    banana= createSprite(660, Math.round(random(120, 200)), 10, 10);
+    banana= createSprite(620, Math.round(random(120, 200)), 10, 10);
     banana.addImage("banana", bananaImage);
     banana.scale= 0.1;
     banana.lifetime= 180;
     banana.velocityX= -5;
     fruitsGroup.add(banana);
-   // banana.frameCount
   }
 }
 
@@ -115,5 +111,6 @@ function obstacles() {
     obstacle.lifetime= 200;
     obstacle.velocityX= -4.5;
     obstaclesGroup.add(obstacle);
+    obstacle.setCollider("circle", -80, 0, 240);
   }
 }
